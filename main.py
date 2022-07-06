9d85a1c9aef4108b5754d4559195b2d2529d343b
atakux <rockyangela5@gmail.com>
2022-07-06 13:53:54 -0700

    added comments

--- main.py
+++ main.py
@@ -8,10 +8,12 @@ from pprint import pprint
 
 
 def miles_to_metres(miles):
+    """converts miles to metres, returns measurement in metres"""
     return miles*1609.344
 
 
 def weather_api(city):
+    """access the weatherapi and return the response/data"""
     # get weather api info using the cityName
     weather_url = 'https://api.weatherapi.com/v1/current.json'
     weather_query = {"q": city}
@@ -25,10 +27,12 @@ def weather_api(city):
 
 
 def get_location_ip():
+    """returns user location name based on their ip address"""
     weather_response = weather_api('auto:ip')
 
     the_city = ''
 
+    # parse through weatherapi response data to retrieve location name
     for key, val in weather_response.json().items():
         if key == 'location':
             for k, v in val.items():
@@ -40,10 +44,12 @@ def get_location_ip():
 
 
 def get_location_zip(zip_code):
+    """returns user location name based on their zipcode"""
     weather_response = weather_api(zip_code)
 
     the_city = ''
 
+    # parse through weatherapi response data to retrieve location name
     for key, val in weather_response.json().items():
         if key == 'location':
             for k, v in val.items():
@@ -55,16 +61,21 @@ def get_location_zip(zip_code):
 
 
 def get_weather(city):
+    """retrieves temp, condition, feels temp, and winds based on user city
+       returns formatted sentence displaying gathered info"""
+
     weather_response = weather_api(city)
 
     if city == 'auto:ip':
         city = get_location_ip()
 
+    # empty variables for storage
     temp = ''
     sky = ''
     feels_temp = ''
     winds = ''
 
+    # parse through weatherapi response data to gather info
     for key, val in weather_response.json().items():
         if key == 'location':
             for k, v in val.items():
@@ -92,7 +103,7 @@ def get_weather(city):
 
 
 def coordinates(city):
-
+    """returns coordinates based on user location"""
     weather_response = weather_api(city)
 
     # empty list to store longitude, latitude in that order
@@ -113,12 +124,15 @@ def coordinates(city):
 
 
 def categories():
+    """prompts user for categorical input to send to place api. returns category"""
     category_list = ['accommodation', 'activity', 'beach', 'commercial', 'catering', 'entertainment', 'leisure']
     option = input("if you would like to see a list of categories\nhit enter, otherwise input a category: ").lower()
 
+    # check for invalid input
     if type(option) != str:
         print("invalid input")
     elif option == '':
+        # display menu since user hit enter
         print("here is a list of categories to choose from: ")
         for i in category_list:
             print(i, end='\n')
@@ -135,7 +149,10 @@ def categories():
 
 
 def places_api(city, rad):
+    """access the placesapi and return response/data"""
     radius = miles_to_metres(rad)
+
+    # prompt user for amount of locations, if invalid use default
     how_many = int(input("how many places would you like listed? "))
     # set a maximum number of places
     if how_many > 20:
@@ -145,17 +162,20 @@ def places_api(city, rad):
         print("the number of places must be at least 1. \ndefaulting to 1 place")
         how_many = 1
 
+    # prompt the user to choose a category
     category = categories()
 
+    # retrieve user coordinates
     lon_lat = coordinates(city)
 
+    # store coordinates in separate variables
     longitude = lon_lat[1]
     latitude = lon_lat[0]
 
     headers = CaseInsensitiveDict()
     headers["Accept"] = "application/json"
 
-    # get places api info using the long and lat from weather api
+    # set up the url based on user input and base links
     main_url = "https://api.geoapify.com/v2/places?"
     category_url = "categories="+category
     coord_url = "&filter=circle:"+str(longitude)+","+str(latitude)+","+str(radius)
@@ -170,17 +190,23 @@ def places_api(city, rad):
 
 
 def db_print():
+    """prompt user for starter info. call other functions to retrieve information. add information into database.
+       print information for the user
+    """
     # receive user input for the city they would like weather for
     city_name = input("input a city or zipcode to get weather, [leave blank if you want your IP to be inputted "
                       "for you]: ")
+    # check if the user wants to use their ip or zipcode instead of city name
     if city_name == '':
         print(f"...retrieving your IP... location = {get_location_ip()}")
         city_name = 'auto:ip'
     elif city_name.isdigit():
         print(f"your location at the zipcode {city_name} is {get_location_zip(city_name)}")
 
+    # prompt user for radius in miles
     miles_radius = int(input("how many miles radius? "))
 
+    # set max and min radius, default values if invalid
     if miles_radius > 30:
         print("\nmax radius is 30 miles. \ndefaulting to 30 miles.")
         miles_radius = 30
@@ -188,20 +214,26 @@ def db_print():
         print("\nmin radius is 1 mile. \ndefaulting to 1 mile.")
         miles_radius = 1
 
+    # checking for invalid city_name input
     try:
+        # call places_api to get places
         places_response = places_api(city_name, miles_radius)
+
+        # check if the city_name was a zip code or blank, rather than a city name
         if city_name == 'auto:ip':
             city_name = get_location_ip()
         elif city_name.isdigit():
             city_name = get_location_zip(city_name)
 
+        # display the current weather conditions for the city
         current_weather = get_weather(city_name)
         print(f"The weather in {city_name}: \n\t{current_weather}")
 
-        # Storing in database
+        # storing in database
         engine = db.create_engine('sqlite:///activity_db.db')
         places = places_response.json()["features"]
 
+        # printing
         print('Place Name \t\t\t\t\t Address')
         print('---------- \t\t\t\t\t -------')
         for place in places:
@@ -220,6 +252,8 @@ def db_print():
 
             # result = engine.execute('SELECT * FROM Activity;').fetchall()
             # print(pd.DataFrame(result))
+
+    # if user inputted invalid city_name display error message
     except:
         print("\nan error occurred.\nplease run the program again and be sure your input is correct.")
 
